57e73f50a9f44f62bd67023db5a9598237b979d9
Rusbell Lozano <ruslllozano@gmail.com>
2021-03-17 13:53:27 -0500

    fix warning

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,18 +1,21 @@
 import initFirebase from '../database'
 import ListProducts from '../components/ListProducts'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext} from 'react';
 import ExitToApp from '@material-ui/icons/ExitToApp';
+import { AuthContext } from "../auth";
 import {
     Button
 } from '@material-ui/core';
 function Home() {
+    const {currentUser} = useContext(AuthContext);
     const [products, setProducts] = useState([])
     const fetchProducts = async () => {
         const response = initFirebase.firestore().collection('products');
         const data = await response.get();
-        data.docs.forEach(item => {
-            setProducts([...products, item.data()])
-        })
+        setProducts(data.docs.map(doc => doc.data()))
+        // data.docs.forEach(item => {
+        //     setProducts([...products, item.data()])
+        // })
     }
     useEffect(() => {
         fetchProducts();
@@ -29,6 +32,7 @@ function Home() {
                     Sign out
                 </Button>
                 <h1>Home</h1>
+                <p>Logged as: {currentUser.email}</p>
                 <Button variant="contained" color="primary">Add product</Button>
 
                 {
